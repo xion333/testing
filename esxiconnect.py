5f88f5cdce807c198b4be89be2b57a30193e8c50
xf.imonaboat@gmail.com <xf.imonaboat@gmail.com>
2017-11-26 21:52:48 -0800

    PW Removal

--- esxiconnect.py
+++ esxiconnect.py
@@ -6,7 +6,7 @@ server = VIServer()
 
 try:
     ssl._create_default_https_context=ssl._create_unverified_context
-    server.connect("s3cn3t.ddns.net", "Socialoutcast", "$Lack3r0")
+    server.connect("s3cn3t.ddns.net", "", "")
     print "Connected to {}{}".format(server.get_server_type(),server.get_api_version())
 finally:
     ssl._create_default_https_context = default_context
