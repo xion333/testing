09b297702969b7d9906be00061816f57e182ef72
xf.imonaboat@gmail.com <xf.imonaboat@gmail.com>
2017-11-26 21:38:42 -0800

    eSXI connection

---
+++ esxiconntect.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import ssl
+from pysphere import VIServer
+default_context=ssl._create_default_https_context
+server = VIServer()
+
+try:
+    ssl._create_default_https_context=ssl._create_unverified_context
+    server.connect("s3cn3t.ddns.net", "Socialoutcast", "$Lack3r0")
+    print "Connected to {}{}".format(server.get_server_type(),server.get_api_version())
+finally:
+    ssl._create_default_https_context = default_context
